78999137ab81e19c8ecaaf9e0f8f274f699379a3
Sergiy Matsiuk <sergiymatsiuk1997@gmail.com>
2021-10-05 20:19:53 +0300

    add register step2

---
+++ components/register/RegisterStep2.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Step 2</h2>
+</template>
--- pages/register.vue
+++ pages/register.vue
@@ -3,15 +3,19 @@
     <register-step1
       v-if="!next"
       @next-step="next=true"/>
+    <register-step2
+      v-else/>
   </div>
 </template>
 
 <script>
 import RegisterStep1 from '@/components/register/RegisterStep1'
+import RegisterStep2 from '@/components/register/RegisterStep2'
 
 export default {
   components: {
-    RegisterStep1
+    RegisterStep1,
+    RegisterStep2
   },
   data () {
     return {
