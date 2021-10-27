ec24326a6682a372819c544a039813a31ad3e148
Sergiy Matsiuk <sergiymatsiuk1997@gmail.com>
2021-10-27 21:01:52 +0300

    add Box News to News page

--- components/news/NewsShow.vue
+++ components/news/NewsShow.vue
@@ -1,24 +1,45 @@
 <template>
-  <transition-group name="news-list" tag="div" class="news-list">
-    <news-card
-      v-for="item in news"
-      :key="item.id"
-      :item="item"/>
+  <transition-group name="news-list" tag="div">
+    <div class="news-list" v-for="blockNews in arrOfNews" :key="blockNews[0].id">
+      <news-card
+        v-for="item in blockNews.slice(0, 2)"
+        :key="item.id"
+        :item="item"/>
+      <news-card-small
+        v-for="item in blockNews.slice(2, 6)"
+        :key="item.id"
+        :item="item"/>
+    </div>
   </transition-group>
 </template>
 
 <script>
 import NewsCard from '@/components/news/NewsCard'
+import NewsCardSmall from '@/components/news/NewsCardSmall'
 
 export default {
   name: 'news-show',
   components: {
-    NewsCard
+    NewsCard, NewsCardSmall
+  },
+  data () {
+    return {
+      size: 6
+    }
   },
   props: {
     news: {
       type: Array,
       default: []
+    }
+  },
+  computed: {
+    arrOfNews () {
+      let arrNews = []
+      for ( let i = 0; i<Math.ceil(this.news.length/this.size); i++) {
+        arrNews.push(this.news.slice((i*this.size), (i*this.size)+this.size))
+      }
+      return arrNews
     }
   }
 }
@@ -29,6 +50,7 @@ export default {
     display: flex;
     flex-direction: column;
     gap: 16px;
+    margin-bottom: 24px;
   }
   @media (min-width: 1124px) {
     .news-list {
